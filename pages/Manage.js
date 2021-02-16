43ebebc84392134fa3098447b499e1a9f38600a2
MD2990 <maz2826maz@gmail.com>
2021-02-16 14:11:05 +0400

    date

--- pages/Manage.js
+++ pages/Manage.js
@@ -8,8 +8,13 @@ import Button from 'react-bootstrap/Button';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, InputGroup, FormControl } from 'react-bootstrap';
+import { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { FcEditImage } from 'react-icons/fc';
 
 export default function Manage() {
+	const [startDate, setStartDate] = useState(new Date());
 	return (
 		<>
 			<Link href='/'>
@@ -20,60 +25,77 @@ export default function Manage() {
 
 			<Jumbotron>
 				<Form>
-					<Row className='justify-content-md-center mb-4'>
+					<Row className=' justify-content-center align-items-center mb-4'>
 						{' '}
-						<Col xm sm='4'>
+						<Col xm='true' sm='4'>
 							{' '}
 							<Form.Label className={styles.label_text}> Make</Form.Label>
 							<Form.Control size='lg' placeholder='---' />
 						</Col>
-						<Col xm sm='4'>
+						<Col xm='true' sm='4'>
 							{' '}
 							<Form.Label className={styles.label_text}>Model</Form.Label>
 							<Form.Control size='lg' placeholder='---' />
 						</Col>
 					</Row>
-					<Row className='justify-content-md-center mb-4'>
+					<Row className=' justify-content-center align-items-center mb-4'>
 						{' '}
-						<Col xm sm='4'>
+						<Col xm='true' sm='4'>
 							{' '}
 							<Form.Label className={styles.label_text}>Year</Form.Label>
 							<Form.Control size='lg' placeholder='---' />
 						</Col>
-						<Col xm sm='4'>
+						<Col xm='true' sm='4'>
 							{' '}
 							<Form.Label className={styles.label_text}>VIN</Form.Label>
 							<Form.Control size='lg' placeholder='---' />
 						</Col>
 					</Row>{' '}
-					<Row className='justify-content-md-center mb-5'>
+					<Row className=' justify-content-center align-items-center mb-4'>
 						{' '}
-						<Col xm sm='4'>
+						<Col xm='true' sm='4'>
 							{' '}
 							<Form.Label className={styles.label_text}>Color</Form.Label>
 							<Form.Control size='lg' placeholder='---' />
 						</Col>
-						<Col xm sm='4'>
+						<Col xm='true' sm='4'>
 							{' '}
 							<Form.Label className={styles.label_text}>Mileage</Form.Label>
 							<Form.Control size='lg' placeholder='---' />
 						</Col>
 					</Row>
-					<Row className='justify-content-md-center mb-5'>
+					<Row className=' justify-content-center align-items-center mb-4'>
 						{' '}
-						<Col xm sm='4'>
+						<Col xm='true' sm='4'>
 							{' '}
 							<Form.Label className={styles.label_text}>
 								Rental Fees{' '}
 							</Form.Label>
 							<Form.Control size='lg' placeholder='---' />
 						</Col>
-						<Col xm sm='4'>
+						<Col xm='true' sm='4'>
 							{' '}
 							<Form.Label className={styles.label_text}>
 								Registration Expiry Date
 							</Form.Label>
-							<Form.Control size='lg' placeholder='---' />
+							{/* 							<Form.Control size='lg' placeholder='---' />
+				<DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+
+ */}{' '}
+							<Form.Control
+								type='date'
+								className='form-control'
+								size='lg'
+								placeholder='---'
+								onChange={(e) => {
+									console.log(e.target.value);
+								}}
+							/>
+							{/* 	<DatePicker
+								selected={startDate}
+								onChange={(date) => setStartDate(date)}
+								className={`${styles.input_text} 	form-control `}
+							/> */}
 						</Col>
 					</Row>
 					{/* 	<Form.Label>Email</Form.Label>
@@ -94,8 +116,16 @@ export default function Manage() {
 						</Form.Control>
 
 						<Form.Check type='checkbox' label='Check me out' /> */}
-					<Row className='justify-content-center ml-4 mr-4'>
-						<Button variant='success' type='submit' size='lg' block>
+					<Row
+						md={6}
+						className='justify-content-center align-items-center ml-6 mr-11'>
+						<Button
+							variant='success'
+							type='submit'
+							size='lg'
+							block
+							className='p-3 font-weight-bold text-uppercase 
+							'>
 							Submit
 						</Button>
 					</Row>
